e6fe68e34865e1ad94b98d772f32a13775d180fd
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-08-30 04:04:13 +0200

    added task 2 and corrected  variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Prompt user for 2 tasks with (title, description, status)
 
 //=======Task 1 ========
-let title = prompt("Enter Task 1 title:");
-let description =prompt("Enter Task 1 description:");
-let status =prompt("Enter Task 1 status:");
+let Task1Title = prompt("Enter Task 1 title:");
+let Task1Description =prompt("Enter Task 1 description:");
+let task1Statusstatus =prompt("Enter Task 1 status:");
 
 //Converting status inputs to lowercase
 task1Status = task1Status.toLowerCase();
@@ -11,3 +11,8 @@ task1Status = task1Status.toLowerCase();
 //Validation for status input
 let task1Status = prompt("Enter Task 1 status (todo, doing, done):");
 alert("Invalid status! Please enter todo, doing, or done.");
+
+//=======Task 2 ========
+let Task2Title = prompt("Enter Task 2 title:");
+let Task2Description =prompt("Enter Task 2 description:");
+let Task2Status =prompt("Enter Task 2 status:");
